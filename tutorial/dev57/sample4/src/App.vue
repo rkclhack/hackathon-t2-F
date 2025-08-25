543ad7c6fb63d41f8e4437466d67e202c61a4f2f
iwasaki2 <otiru929@gmail.com>
2025-08-25 14:31:26 +0900

    [2.4] イベント

--- tutorial/dev57/sample4/src/App.vue
+++ tutorial/dev57/sample4/src/App.vue
@@ -2,13 +2,16 @@
 const handleClick = () => {
   alert("ボタンがクリックされました")
 }
+
+const handleMouseOver = () => {
+  alert("ボタンがマウスオーバーされました")
+}
 </script>
 
 <template>
   <h1>Sample4</h1>
-  <button @click="handleClick">click</button>
+  <button @click="handleClick">Click</button>
+  <button @mouseover="handleMouseOver">MouseOver</button>
 </template>
-
-
 
 
