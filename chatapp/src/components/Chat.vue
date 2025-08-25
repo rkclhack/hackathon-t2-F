3dcfcc5a4d51d4d92b73183de7cb0915386f68ca
iwasaki2 <otiru929@gmail.com>
2025-08-25 15:54:50 +0900

    [chat] Add memo feature (visible only to myself)

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -36,10 +36,11 @@ const onExit = () => {
 
 // メモを画面上に表示する
 const onMemo = () => {
+  if (!chatContent.value.trim()) return//もし、入力欄が空文字やスペースだけだった場合は 何もしないで終了。
   // メモの内容を表示
-
+  chatList.unshift(`${userName.value}さんのメモ：「${chatContent.value}」`)
   // 入力欄を初期化
-
+  chatContent.value = ""
 }
 // #endregion
 
@@ -86,10 +87,10 @@ const registerSocketEvent = () => {
     <h1 class="text-h3 font-weight-medium">Vue.js Chat チャットルーム</h1>
     <div class="mt-10">
       <p>ログインユーザ：{{ userName }}さん</p>
-      <textarea variant="outlined" placeholder="投稿文を入力してください" rows="4" class="area"></textarea>
+      <textarea v-model="chatContent" variant="outlined" placeholder="投稿文を入力してください" rows="4" class="area"></textarea>
       <div class="mt-5">
-        <button class="button-normal">投稿</button>
-        <button class="button-normal util-ml-8px">メモ</button>
+        <button class="button-normal" @click="onPublish">投稿</button>
+        <button class="button-normal util-ml-8px" @click="onMemo">メモ</button>
       </div>
       <div class="mt-5" v-if="chatList.length !== 0">
         <ul>
