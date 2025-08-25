7406f3aa6e2c32dc4bb9d14e071e4db759c2011b
takapom <23610119yt@stu.yamato-u.ac.jp>
2025-08-25 15:37:55 +0900

    投稿内容表示させました

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -21,12 +21,13 @@ onMounted(() => {
 })
 // #endregion
 
-// #region browser event handler
-// 投稿メッセージをサーバに送信する
 const onPublish = () => {
-
-  // 入力欄を初期化
-
+  if (chatContent.value.trim() === "") return
+  socket.emit("publishEvent", {
+    userName: String(userName), // 文字列に変換
+    message: chatContent.value,
+  })
+  chatContent.value = ""
 }
 
 // 退室メッセージをサーバに送信する
@@ -46,7 +47,6 @@ const onMemo = () => {
 // #region socket event handler
 // サーバから受信した入室メッセージ画面上に表示する
 const onReceiveEnter = (data) => {
-  chatList.push()
 }
 
 // サーバから受信した退室メッセージを受け取り画面上に表示する
@@ -56,7 +56,7 @@ const onReceiveExit = (data) => {
 
 // サーバから受信した投稿メッセージを画面上に表示する
 const onReceivePublish = (data) => {
-  chatList.push()
+  chatList.push({ userName: data.userName, message: data.message })
 }
 // #endregion
 
@@ -75,7 +75,8 @@ const registerSocketEvent = () => {
 
   // 投稿イベントを受け取ったら実行
   socket.on("publishEvent", (data) => {
-
+    onReceivePublish(data)
+    console.log(data)
   })
 }
 // #endregion
@@ -86,15 +87,15 @@ const registerSocketEvent = () => {
     <h1 class="text-h3 font-weight-medium">Vue.js Chat チャットルーム</h1>
     <div class="mt-10">
       <p>ログインユーザ：{{ userName }}さん</p>
-      <textarea variant="outlined" placeholder="投稿文を入力してください" rows="4" class="area"></textarea>
+      <textarea v-model="chatContent" variant="outlined" placeholder="投稿文を入力してください" rows="4" class="area"></textarea>
       <div class="mt-5">
-        <button class="button-normal">投稿</button>
+        <button @click="onPublish" class="button-normal">投稿</button>
         <button class="button-normal util-ml-8px">メモ</button>
       </div>
       <div class="mt-5" v-if="chatList.length !== 0">
-        <ul>
-          <li class="item mt-4" v-for="(chat, i) in chatList" :key="i">{{ chat }}</li>
-        </ul>
+     <li class="item mt-4" v-for="(chat, i) in chatList" :key="i">
+  <strong>{{ chat.userName }}:</strong> {{ chat.message }}
+     </li>
       </div>
     </div>
     <router-link to="/" class="link">
@@ -102,6 +103,7 @@ const registerSocketEvent = () => {
     </router-link>
   </div>
 </template>
+
 
 <style scoped>
 .link {
